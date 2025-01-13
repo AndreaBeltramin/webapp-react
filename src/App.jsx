c2a69137d7ed27a25720d1c4755b1878334596f4
AndreaBeltramin <andreabeltra99@gmail.com>
2025-01-13 15:17:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+	return (
+		<>
+			<div className="container my-5">
+				<h1>La mia webapp</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
